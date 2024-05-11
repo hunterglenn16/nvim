bbc369f7c56e3e0310ba319931d7f461073a13ea
hunterglenn16 <36973534+hunterglenn16@users.noreply.github.com>
2024-05-11 15:19:12 -0400

    forced lang to en
    and lifted tutor removal

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
@@ -39,7 +39,6 @@ require("lazy").setup({
         -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
-        "tutor",
         "zipPlugin",
       },
     },
