9939f45e92379132ca5a6b6b2f255cce270d618f
hunterglenn16 <36973534+hunterglenn16@users.noreply.github.com>
2024-05-25 20:13:31 -0400

    Added plugin and keymaps
    Added undotree
    added p commands for faster motions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--https://github.com/hunterglenn16/nvim.git Add any additional keymaps here
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+  builtin.grep_string({ search = vim.fn.input("Grep >") })
+end)
+vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle)
